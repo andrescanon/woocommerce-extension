75a6ed63b55eead2cc5a8ee177378b7c789a7ac3
Starrimus <starrimus7@gmail.com>
2018-10-09 13:00:44 +0300

    test conf #9

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,13 +11,3 @@
 $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'woocommerce-extension/stacc-recommendation.php' ),
 );
-
-// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
-// location. Otherwise, we'll just assume that this plugin is installed in a
-// WordPress develop SVN checkout.
-
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . 'bootstrap.php';
-} else {
-	require 'bootstrap.php';
-}
