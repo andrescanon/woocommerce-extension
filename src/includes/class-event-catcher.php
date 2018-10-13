c2b4e874bd7aef678f1db2e11519be00524f6c7f
Martin <martin457345@gmail.com>
2018-10-13 17:19:01 +0300

    Processing product view, search, add-to-cart and purchase events, see #16, see #17, see #18, see #19

--- src/includes/class-event-catcher.php
+++ src/includes/class-event-catcher.php
@@ -5,7 +5,7 @@
  *
  * Defines the plugin name, version and hooks, filter for catching events
  *
- * @since      0.1.0
+ * @since      0.2.0
  * @package    Recommendations
  * @subpackage Recommendations/includes
  * @author     Martin Jürgel <martin457345@gmail.com>
@@ -13,11 +13,10 @@
 
 class Event_Catcher
 {
-
     /**
      * The ID of this plugin.
      *
-     * @since      0.1.0
+     * @since      0.2.0
      * @access     private
      * @var        string $plugin_name The ID of this plugin.
      */
@@ -26,7 +25,7 @@ class Event_Catcher
     /**
      * The version of this plugin.
      *
-     * @since      0.1.0
+     * @since      0.2.0
      * @access     private
      * @var        string $version The current version of this plugin.
      */
@@ -35,7 +34,7 @@ class Event_Catcher
     /**
      * Initialize the class and set its properties.
      *
-     * @since      0.1.0
+     * @since      0.2.0
      * @param      string $plugin_name The name of this plugin.
      * @param      string $version The version of this plugin.
      */
@@ -50,7 +49,7 @@ class Event_Catcher
     /**
      * Callback for catching search events.
      *
-     * @since 0.1.0
+     * @since       0.2.0
      * @param       WP_Query $query Object containing query data.
      * @return      WP_Query $query Object containing query data.
      */
@@ -58,8 +57,17 @@ class Event_Catcher
     {
         if ( $query->is_search )
         {
-            $args = implode(',', $query->query);
-            error_log("Search, args: $args");
+            $query_string = get_search_query(true);
+            //$args = $query->query;
+            //TODO retrieve filters
+            $data = [
+                'stacc_id' => get_current_user_id(),
+                'query' => $query_string,
+                'filters' => [],
+                'website' => get_site_url(),
+                'properties' => []
+            ];
+            Recommender_API::get_instance()::send_event($data, 'search');
         }
         return $query;
     }
@@ -67,39 +75,86 @@ class Event_Catcher
     /**
      * Callback for catching add to cart events.
      *
-     * @since 0.1.0
-     * @param $cart_item_key The cart item key.
-     * @param $product_id ID of the product being added to cart.
-     * @param $quantity Quantity selected by the user.
-     * @param $variation_id The variation ID.
-     * @param $variation The variation.
-     * @param $cart_item_data Cart item data.
+     * @since      0.2.0
+     * @param      string $cart_item_key The cart item key.
+     * @param      int $product_id ID of the product being added to cart.
+     * @param      int $quantity Quantity selected by the user.
+     * @param      int $variation_id The variation ID.
+     * @param      WC_Product_Variation $variation The variation.
+     * @param      array $cart_item_data Cart item data.
      */
     public function action_woocommerce_add_to_cart( $cart_item_key, $product_id, $quantity, $variation_id, $variation, $cart_item_data )
     {
-        error_log("Add to cart, ID: $product_id");
-
+        $properties =  [
+            'categories' => wc_get_product_category_list($product_id),
+            'stock_status' => wc_get_product($product_id)->get_stock_status()
+        ];
+        $data = [
+            'item_id' => $product_id,
+            'stacc_id' => get_current_user_id(),
+            'website' => get_site_url(),
+            'properties' => $properties
+        ];
+        Recommender_API::get_instance()::send_event($data, 'add');
     }
 
     /**
      * Callback for catching product view events.
      *
-     * @since 0.1.0
+     * @since      0.2.0
      */
     public function action_woocommerce_single_product_summary()
     {
         global $product;
         $id = $product->get_id();
-        error_log("Product view, ID: $id");
+
+        $properties =  [
+            'categories' => wc_get_product_category_list($id),
+            'stock_status' => $product->get_stock_status()
+        ];
+
+        $data = [
+            'item_id' => $id,
+            'stacc_id' => get_current_user_id(),
+            'website' => get_site_url(),
+            'properties' => $properties
+        ];
+        Recommender_API::get_instance()::send_event($data, 'view');
     }
 
     /**
      * Callback for catching payment complete events.
      *
-     * @since 0.1.0
-     * @param $order_id Order ID.
+     * @since      0.2.0
+     * @param      int $order_id Order ID.
      */
-    public function action_woocommerce_payment_complete( $order_id ) {
-        error_log( "Payment complete, order ID: $order_id" );
+    public function action_woocommerce_payment_complete( $order_id )
+    {
+        $order = wc_get_order( $order_id );
+        $items = $order->get_items();
+        $item_list = array();
+        foreach ( $items as $item )
+        {
+            $product_id = $item->get_id();
+            $product_quantity = $item->get_quantity();
+            $price = $order->get_item_total($item);
+            $item_arr =  [
+                'item_id' => $product_id,
+                'quantity' => $product_quantity,
+                'price' => $price
+            ];
+            array_push($item_list, $item_arr);
+        }
+        $currency = $order->get_currency();
+
+
+        $data = [
+            'stacc_id' => get_current_user_id(),
+            'item_list' => $item_list,
+            'website' => get_site_url(),
+            'currency' => $currency,
+            'properties' => []
+        ];
+        Recommender_API::get_instance()::send_event($data, 'purchase');
     }
 }
