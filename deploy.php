78abbe6dbabdef5c1ec49218fad42f2f6b79ba88
Stiivo Siider <stiivosiider@gmail.com>
2018-10-14 18:46:54 +0300

    Test push v5 see #9

--- deploy.php
+++ deploy.php
@@ -1,5 +1,5 @@
 <?php
-//TEST PUSH v4
+//TEST PUSH v5
 // Forked from https://gist.github.com/1809044
 // Available from https://gist.github.com/nichtich/5290675#file-deploy-php
 $TITLE   = 'Git Deployment Hamster';
