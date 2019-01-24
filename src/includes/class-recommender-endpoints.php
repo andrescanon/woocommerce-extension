d984d469dfe1ca3d8f7a6df018ab238ee4f7a00c
Starrimus <starrimus7@gmail.com>
2019-01-24 19:40:21 +0200

    Update class-recommender-endpoints.php

--- src/includes/class-recommender-endpoints.php
+++ src/includes/class-recommender-endpoints.php
@@ -70,41 +70,56 @@ class Recommender_Endpoints extends WP_REST_Controller {
         ) );
     }
 
-    /**
-     * Start the process of syncing products
-     *
-     * @since 0.6.0
-     * @param WP_REST_Request $request Full data about the request.
-     * @return WP_REST_Response
-     */
     public function sync_products( $request ) {
-	    $handle = $this->handle_params("products", $request);
-	    if (!is_bool($handle))
-		    return $handle;
-
-	    Recommender_WC_Log_Handler::get_instance()::logNotice("Product syncing started!");
-	    Recommender_Syncer::get_instance()->sync_products();
-	    Recommender_WC_Log_Handler::get_instance()::logNotice("Product syncing done!");
-	    return new WP_REST_Response( array(), 200 );
+        $handle = $this->handle_params("products", $request);
+        if (!is_bool($handle))
+                return $handle;
+        Recommender_WC_Log_Handler::get_instance()::logNotice("Product syncing started!");
+        $success = Recommender_Syncer::get_instance()->sync_products();
+        if (!$success)
+        {
+                Recommender_WC_Log_Handler::get_instance()::logError("Log syncing failed!");
+                return new WP_REST_Response( array(), 500 );
+        }
+        Recommender_WC_Log_Handler::get_instance()::logNotice("Product syncing done!");
+        return new WP_REST_Response( array(), 200 );
     }
+
+
 
     /**
-     * Start the process of syncing logs
-     *
-     * @since 0.6.0
-     * @param WP_REST_Request $request Full data about the request.
-     * @return WP_REST_Response
-     */
-    public function sync_logs( $request ) {
-	    $handle = $this->handle_params("logs", $request);
-	    if (!is_bool($handle) || !$handle)
-	    	return $handle;
 
-	    Recommender_WC_Log_Handler::get_instance()::logNotice("Log syncing started!");
-	    Recommender_Syncer::get_instance()->sync_logs();
-	    Recommender_WC_Log_Handler::get_instance()::logNotice("Log syncing done!");
-	    return new WP_REST_Response( array(), 200 );
+     * Start the process of syncing logs
+
+     *
+
+     * @since 0.6.0
+
+     * @param WP_REST_Request $request Full data about the request.
+
+     * @return WP_REST_Response
+
+     */
+
+    public function sync_logs( $request ) {
+        $handle = $this->handle_params("logs", $request);
+        if (!is_bool($handle) || !$handle)
+        	return $handle;
+	    
+        Recommender_WC_Log_Handler::get_instance()::logNotice("Log syncing started!");
+        $success = Recommender_Syncer::get_instance()->sync_logs();
+        if (!$success)
+        {
+                Recommender_WC_Log_Handler::get_instance()::logError("Log syncing failed!");
+                return new WP_REST_Response( array(), 500 );
+        }
+	    
+        Recommender_WC_Log_Handler::get_instance()::logNotice("Log syncing done!");
+        return new WP_REST_Response( array(), 200 );
     }
+
+
+
 
 	/**
 	 * Handles the parameters of the request
